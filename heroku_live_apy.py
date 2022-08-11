eadc8fe243ba563e9fdf4a8bf9bbbe4e3459d451
leoMurtha <aka.murtha@gmail.com>
2022-08-11 20:08:33 -0300

    finishing some cleaning

--- heroku_live_apy.py
+++ heroku_live_apy.py
@@ -2,35 +2,30 @@
 Test the live prediction endpoint on Heroku
 """
 import requests
-import logging
-
-logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 
 
 features = {
-    "age": 36,
+    "age": 27,
     "workclass": "Private",
-    "fnlgt": 302146,
-    "education": "HS-grad",
-    "education_num": 9,
-    "marital_status": "Divorced",
-    "occupation": "Craft-repair",
-    "relationship": "Husband",
+    "fnlgt": 77516,
+    "education": "Masters",
+    "education_num": 13,
+    "marital_status": "Never-married",
+    "occupation": "Tech-support",
+    "relationship": "Unmarried",
     "race": "White",
     "sex": "Male",
-    "capital_gain": 2000,
+    "capital_gain": 1600,
     "capital_loss": 0,
-    "hours_per_week": 45,
-    "native_country": "United-States"
+    "hours_per_week": 46,
+    "native_country": "Cambodia"
 }
 
 
-app_url = "https://udacity-income-mle.herokuapp.com//inference"
+app_url = "https://udacity-income-mle.herokuapp.com/inference"
 
 r = requests.post(app_url, json=features)
 print(r)
 assert r.status_code == 200
 
-logging.info("Testing Heroku app")
-logging.info(f"Status code: {r.status_code}")
-logging.info(f"Response body: {r.json()}")
+print(f"Response body: {r.json()}")
